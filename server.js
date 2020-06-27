97188dc3efcfe9b61d2a8a238b620151fa2c1682
unknown <arne.n.bjerkaas@gmail.com>
2020-06-27 16:47:13 +0200

    Build project and add express server

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var path = require("path");
 var serveStatic = require("serve-static");
 
 var app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(serveStatic(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build"));
+});
 
 var port = process.env.PORT || 8000;
 app.listen(port);
